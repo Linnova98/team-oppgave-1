152aa225d9f559d38fc3c90ffe6d3a4e760e0b42
Linnova <92028792+Linnova98@users.noreply.github.com>
2025-02-08 19:39:56 +0100

    Updated to local images

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const countView = document.getElementById('count');
-let previousElement, previousId, count = 0;
+const countView = document.getElementById("count");
+let previousElement,
+  previousId,
+  count = 0;
 
 function showImage(element, id) {
-    if (previousElement === element) return;
-    if (element.classList.contains('done')) return;
-    if (previousElement) {
-        previousElement.classList.remove('selected');
-        if (previousId !== id) {
-            element.classList.add('selected');
-            previousElement.innerHTML = '';
-            previousElement = element;
-            previousId = id;
-        } else {
-            count++;
-            countView.innerHTML = count;
-            previousElement.classList.add('done');
-            element.classList.add('done');
-            previousElement = undefined;
-            previousId = undefined;
-        }
+  if (previousElement === element) return;
+  if (element.classList.contains("done")) return;
+  if (previousElement) {
+    previousElement.classList.remove("selected");
+    if (previousId !== id) {
+      element.classList.add("selected");
+      previousElement.innerHTML = "";
+      previousElement = element;
+      previousId = id;
     } else {
-        element.classList.add('selected');
-        previousElement = element;
-        previousId = id;
+      count++;
+      countView.innerHTML = count;
+      previousElement.classList.add("done");
+      element.classList.add("done");
+      previousElement = undefined;
+      previousId = undefined;
     }
-    element.innerHTML = `<div class="tile i${id}"></div>`;
+  } else {
+    element.classList.add("selected");
+    previousElement = element;
+    previousId = id;
+  }
+  element.innerHTML = `<div class="tile i${id}"></div>`;
 }
